86fd7bed924acbfb9f7a98d41a5ffe33fe7d742d
Mihai Bivol <mm.bivol@gmail.com>
2012-07-31 03:33:24 +0300

    snipper.h: Add snipper documentation.

--- xapian-core/include/xapian/snipper.h
+++ xapian-core/include/xapian/snipper.h
@@ -10,6 +10,10 @@ namespace Xapian {
 class MSet;
 class Stem;
 
+/** Class used to generate snippets from a given text.
+ *
+ *  For generating a snippet, a MSet is needed to calculate a relevance model.
+ */
 class XAPIAN_VISIBILITY_DEFAULT Snipper {
   public:
 
@@ -24,8 +28,10 @@ class XAPIAN_VISIBILITY_DEFAULT Snipper {
 
     ~Snipper();
 
+    /** Set the stemmer for the Snipper object. */
     void set_stemmer(const Xapian::Stem & stemmer);
 
+    /** Set the MSet and calculate the relevance model according to it. */
     void set_mset(const MSet & mset);
 
     void set_window_size(unsigned int window_size);
@@ -34,6 +40,12 @@ class XAPIAN_VISIBILITY_DEFAULT Snipper {
 
     void set_rm_docno(unsigned int docno);
 
+    /** Generate snippet from given text.
+     *
+     * @param text  The text from which to generate the snippet
+     *
+     * @return	    Text of the snippet relevant to the model from input.
+     */
     std::string generate_snippet(const std::string & text);
 };
 
